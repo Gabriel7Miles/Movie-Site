95e40076e23ab8a3b12d556bbfcd5cfe1a71652b
Gabriel7Miles <Gabrielmiles331@gmail.com>
2026-01-13 13:20:31 +0300

    first commit

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MovieCard = ({ movie:
+{ title, vote_average, release_date, poster_path, original_language }
+ }) => {
+  return (
+    <div className= "movie-card">
+      <img 
+        src={poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : '/no-image-available.png'} 
+        alt={title}
+        />
+    </div>
+  )
+}
+
+export default MovieCard;
---
+++ src/components/spinner.jsx
@@ -0,0 +1,11 @@
+import { Spinner as FlowbiteSpinner} from "flowbite-react";
+
+const Spinner = () => {
+  return (
+    <FlowbiteSpinner 
+      aria-label="Default status example" 
+    />
+  );
+};
+
+export default Spinner;
